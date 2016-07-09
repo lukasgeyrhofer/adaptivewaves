fixed sign bug, fixed factor 2 bug

numerics/solve_travelingwaves_popdens_2d_exptransformed.py
@@ -77,7 +77,7 @@ idx3 = 1./(dx*dx*dx)
 
 const_sdx2 = idx2/6.
 const_tdx  = idx1/3.
-const_ft   = 4./3.
+const_tt   = 2./3.
 
 coeff_xp_yp = (speed*idx3 + 0.5*(speed-mutationrate)*idx2)               * mat_ones + 0.25*(np.outer(lin_ones,s)    + np.outer(s,lin_ones))*idx2
 coeff_xp_y0 = (-speed*idx3 + speed*idx2 + 0.5*(speed-mutationrate)*idx1) * mat_ones + 0.50*(np.outer(lin_ones,s+ds) + np.outer(s,lin_ones))*idx1
@@ -91,7 +91,7 @@ coeff_xm_yp = (-0.50*(speed-mutationrate)*idx2)                          * mat_o
 coeff_xm_y0 = (speed*idx3 + speed*idx2 - 0.5*(speed-mutationrate)*idx1)  * mat_ones - 0.50*(np.outer(lin_ones,s+ds) + np.outer(s,lin_ones))*idx1
 coeff_xm_ym = (-speed*idx3 + 0.5*(speed-mutationrate)*idx2)              * mat_ones + 0.25*(np.outer(lin_ones,s)    + np.outer(s,lin_ones))*idx2
 
-fc = coeff_x0_y0 + 2*np.diag(w)/3. + np.diag(w[:space-1] - w[1:],k=1)/(6.*dx) + np.diag(w[1:] - w[:space-1],k=-1)/(6.*dx)
+fc = coeff_x0_y0 + np.diag(2.*w/3.) + np.diag( (w[:space-1] - w[1:])/(6.*dx) ,k=1) + np.diag( (w[:space-1] - w[1:])/(6.*dx) ,k=-1)
 
 
 # Newton-Raphson iterations
@@ -102,12 +102,12 @@ for i in range(args.maxsteps):
     f += coeff_xm_yp * c2[0:space  ,2:space+2] + coeff_xm_y0 * c2[0:space  ,1:space+1] + coeff_xm_ym * c2[0:space  ,0:space]
     
     # compute c1 for terms on diagonal, assume linear extrapolation
-    b = np.dot(c2[1:space+1,1:space+1],w)
+    b  = np.dot(c2[1:space+1,1:space+1],w)
     bp = np.concatenate([b[:space-1],np.array([2*b[space-1] - b[space-2]])])
     bm = np.concatenate([np.array([2*b[0] - b[1]]),b[1:]])
     
     # apply diagonal terms arising from delta
-    f += np.diag(bp*const_sdx2 + b*const_ft + bm*const_sdx2) 
+    f += np.diag(bp*const_sdx2 + b*const_tt + bm*const_sdx2) 
     f += np.diag(const_tdx * (b[1:]-b[:space-1]),k= 1) + np.diag(-const_sdx2*b[1:space-1],k= 2)
     f += np.diag(const_tdx * (b[1:]-b[:space-1]),k=-1) + np.diag(-const_sdx2*b[1:space-1],k=-2)
     